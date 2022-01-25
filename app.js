b0b0810233cffd79e48f5539879f5d0c49d774a7
rusty <dalegribble@riseup.net>
2022-01-25 07:34:24 -0700

    fix invite role bot role grant add comprehensive logging add persistence

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-// Initialize config
-require('dotenv').config();
+const fs = require("fs/promises");
 
 // Initialize discord.js
 const {Client, Intents} = require("discord.js");
@@ -11,11 +10,15 @@ let roleID = "928547378200465448";
 // Event emitter callbacks
 client.on('ready', async () => {
 	console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Bot ready`);
+
 	let guilds = await client.guilds.fetch();
-		guilds.first().roles.fetch(roleID)
-			.then(role => {
-				whitelisted = role.members.size;
-			});
+	let guild = await guilds.first().fetch();
+
+	guild.roles.fetch(roleID)
+		.then(role => {
+			whitelisted = role.members.size;
+		});
 });
 
 client.on('guildMemberAdd', member => {
@@ -23,33 +26,85 @@ client.on('guildMemberAdd', member => {
 		member.guild.invites.fetch({code: code, force: true})
 			.then(invite => {
 				if (trackedData.uses < invite.uses) {
-					trackedData.uses += 1;
+					console.log(`User ${member.user.tag} joined via invite code ${code} owned by ${invite.inviter.tag}`);
+
+					trackedData.uses += (invite.uses - trackedData.uses);
 					trackedData.invitees.push(member.id);
 				}
 
-				if (trackedData.uses >= 5 && whitelisted < 1111) {
-					member.roles.add(roleID);
-					whitelisted += 1;
+				if (trackedData.uses >= 10 && whitelisted < 1111) {
+					console.log(`User ${invite.inviter.tag} was granted whitelist for inviting 10 users.`)
+
+					member.guild.members.fetch(invite.inviterId)
+						.then(inviterMember => {
+							inviterMember.roles.add(roleID);
+							whitelisted += 1;
+
+							trackedInvites.delete(invite.code);
+						});
 				}
+
+				SaveInvites()
+					.then(() => {});
 			});
 	});
 });
 
 client.on('guildMemberRemove', member => {
-	console.log(member)
+	//console.log(member)
 });
 
 client.on('inviteCreate', invite => {
 	trackedInvites.set(invite.code, {
-		owner: invite.inviterId,
+		inviterId: invite.inviter.id,
 		invitees: [],
 		uses: 0
 	});
+
+	console.log(`Invite ${invite.code} created by ${invite.inviter.tag}`);
+
+	SaveInvites()
+		.then(() => {});
 });
 
 client.on('inviteDelete', invite => {
 	trackedInvites.delete(invite.code);
+
+	SaveInvites()
+		.then(() => {});
 });
 
-// Login bot
-client.login('OTM0NDI2NTA4OTM4ODU0NDEw.Yev6gA.Nu9XDFej00UOVEdfxw3VUlk_XpU');
+function SaveInvites() {
+	return new Promise((resolve, reject) => {
+		let jsonObject = {};
+		trackedInvites.forEach((value, key) => {
+			jsonObject[key] = value;
+		});
+
+		fs.writeFile("./invites.json", JSON.stringify(jsonObject, null, 2))
+			.then(() => {
+				resolve();
+			});
+	});
+}
+
+function LoadInvites() {
+	return new Promise((resolve, reject) => {
+		fs.readFile("./invites.json", {encoding: "utf-8"})
+			.then(result => {
+				let jsonObject = JSON.parse(result);
+
+				for (let key in jsonObject) {
+					trackedInvites.set(key, jsonObject[key]);
+				}
+
+				resolve();
+			});
+	});
+}
+
+LoadInvites()
+	.then(() => {
+		// Login bot
+		client.login('OTM0NDI2NTA4OTM4ODU0NDEw.Yev6gA.Nu9XDFej00UOVEdfxw3VUlk_XpU');
+	});
